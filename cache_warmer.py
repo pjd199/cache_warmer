e940cd1b47d886de6ae02e34b50b75dea316782c
pjd199 <89210268+pjd199@users.noreply.github.com>
2023-06-07 15:25:56 +0100

    refactoring

--- cache_warmer.py
+++ cache_warmer.py
@@ -2,30 +2,36 @@ from bs4 import BeautifulSoup, SoupStrainer
 from time import perf_counter_ns
 from requests import get
 
-root = "https://www.life-baptist.org.uk"
+def main() -> None:
+    root = "https://www.life-baptist.org.uk"
 
-response = get(root)
-if response.status_code == 200:
-    print(f"Parsing links for {root}")
-    soup = BeautifulSoup(response.text, "html.parser", parse_only=SoupStrainer("a"))
-    links = {
-        link.attrs["href"]
-        for link in soup.find_all("a")
-        if "href" in link.attrs and link["href"].startswith(root)
-    }
-    for link in sorted(links):
-        start = perf_counter_ns()
-        response = get(link)
-        elapsed = (perf_counter_ns() - start) // 1000000
-        text = (
-            f"{link.replace(root,'')}"
-            f" - status {response.status_code}"
-            f", time {elapsed:,}ms"
-        )
-        if "x-origin-cache-status" in response.headers:
-            text += f", cache: {response.headers['x-origin-cache-status']}"
-        if "x-cdn-cache-status" in response.headers:
-            text += f", cdn: {response.headers['x-cdn-cache-status']}"
-        if elapsed >= 1000:
-            text += ", ****** SLOW *****"
-        print(text)
+    response = get(root)
+    if response.status_code == 200:
+        print(f"Parsing links for {root}")
+        soup = BeautifulSoup(response.text, "html.parser", parse_only=SoupStrainer("a"))
+        links = {
+            link.attrs["href"]
+            for link in soup.find_all("a")
+            if "href" in link.attrs and link["href"].startswith(root)
+        }
+        for link in sorted(links):
+            start = perf_counter_ns()
+            response = get(link)
+            elapsed = (perf_counter_ns() - start) // 1000000
+            text = (
+                f"{link.replace(root,'')}"
+                f" - status {response.status_code}"
+                f", time {elapsed:,}ms"
+            )
+            if "x-origin-cache-status" in response.headers:
+                text += f", cache: {response.headers['x-origin-cache-status']}"
+            if "x-cdn-cache-status" in response.headers:
+                text += f", cdn: {response.headers['x-cdn-cache-status']}"
+            if elapsed >= 1000:
+                text += ", ****** SLOW *****"
+            print(text)
+    else:
+        print(f"Unable to download {root}")
+
+if __name__ == "__main__":
+    main()
